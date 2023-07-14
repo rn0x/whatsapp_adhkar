3be949059f6aa7303037d86aa1ad28674440006d
rn0x <76129163+rn0x@users.noreply.github.com>
2023-07-14 05:01:03 -0700

    Update index.js

--- module/searchQuran/index.js
+++ module/searchQuran/index.js
@@ -2,19 +2,16 @@ import fs from 'fs-extra';
 import path from 'path';
 import { launch } from 'puppeteer';
 import crateHtml from './crateHtml.js';
-import system_distros from "../system_distros.js";
-
 
 export default async function screenshot({ quran: quran, tafser1: tafser1, tafser2: tafser2, english: english, name: name, username: username, filename: filename }) {
 
     await crateHtml({ quran: quran, tafser1: tafser1, tafser2: tafser2, english: english, name: name, username: username }).then(async () => {
 
-        const system = await system_distros();
         let __dirname = path.resolve();
         let config = fs.readJSONSync(path.join(__dirname, './config.json'));
         let browser = await launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            executablePath: process.platform === "win32" || process.platform === "win64" ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" : process.platform === "linux" && system?.id !== 'alpine' ? "/usr/bin/google-chrome-stable" : process.platform === "darwin" ? "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" : config?.executablePath
+            executablePath: config?.executablePath
         }).catch(error => console.log(error));
         let page = await browser.newPage();
         let html = `file:${path.join(__dirname, './module/searchQuran/index.html')}`;
